f6ef3a2d664d10df5eec2da9f579df5d11ff4718
Dominic Torres <dtorres243@west-mec.org>
2021-04-07 18:25:51 -0700

    yeye

--- calc.js
+++ calc.js
@@ -1,28 +1,51 @@
-let calculator = {
-    sum(){
-        return this.a + this.b;
-    },
+// let calculator = {
+//     sum(){
+//         return this.a + this.b;
+//     },
 
-    mul(){
-        return this.a * this.b;
-    },
+//     mul(){
+//         return this.a * this.b;
+//     },
 
-    read(){
-        this.a = +prompt('a?', 0);
-        this.b = +prompt('b?', 0);
-    }
-};
+//     read(){
+//         this.a = +prompt('a?', 0);
+//         this.b = +prompt('b?', 0);
+//     }
+// };
 
-calculator.read();
-alert(calculator.sum());
-alert(calculator.mul());
+// calculator.read();
+// alert(calculator.sum());
+// alert(calculator.mul());
 
 
 let ladder = {
+    step: 0,
+    stepMax: 5,
 
+    // height(){
+    //     console.log(this.stepMax)
+    // },
+
+    up(){
+        if(this.step !== this.stepMax){
+            this.step++;
+        } else{
+            alert("max step reached");
+        }
+    },
+    down(){
+        this.step --;
+    },
+    showStep: function(){
+        console.log(this.step);
+    }
 };
 
 ladder.up(); //go up one step 
 ladder.up(); 
+ladder.up(); 
+ladder.up(); 
+ladder.up(); 
 ladder.down(); //go down 1 step
-ladder.showstep(); //show what step you are on. 1 in this case
+ladder.height(5) //will not increase passed 5
+ladder.showStep(); //show what step you are on. 1 in this case
